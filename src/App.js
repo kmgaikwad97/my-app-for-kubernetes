125b8d31bc29365511ffa7826889925a5e377989
kshitijwohlig <kshitij.gaikwad@wohlig.com>
2024-01-24 01:17:40 +0530

    learning k8

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src="./k8.svg" className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Devops
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.freecodecamp.org/news/learn-kubernetes-in-under-3-hours-a-detailed-guide-to-orchestrating-containers-114ff420e882/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn K8
         </a>
       </header>
     </div>
